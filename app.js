ec3813258482ccfab7efefad8a8ac6067cb305b1
Gaelle Lopez <lope.gaelle@gmail.com>
2022-12-23 18:59:57 +0100

    javascript api

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+KEY_API = "";
+
 // DOM selector
 
 const form = document.querySelector("form");
@@ -6,6 +8,7 @@ const budgetInput = document.querySelector("#budget");
 const themeInput = document.querySelector("#theme");
 const submitBtn = document.querySelector(".form-btn");
 const footer = document.querySelector("footer");
+const ringLoading = document.querySelector(".ring");
 
 const generateSentenceByAgeBudgetAndTheme = (age, budget = "", theme = "") => {
   let sentence = `Suggest me 3 gift ideas for a ${age} years old person`;
@@ -23,6 +26,57 @@ const generateSentenceByAgeBudgetAndTheme = (age, budget = "", theme = "") => {
 // mode loading on
 const setLoadingItems = () => {
   footer.textContent = "Loading ideas in progress..."
+  ringLoading.className = "lds-ring";
+  submitBtn.classList.remove("hover-blc");
+  submitBtn.disabled = true;
 }
 
 // mode loading off
+const removeLoadingItems = () => {
+  submitBtn.className = "hover-blc";
+  ringLoading.classList.remove("lds-ring");
+  submitBtn.disabled = false;
+}
+
+const translateTextToHtml = (text) => {
+text = text.split("\n")
+console.log(text);
+let textHtml = [];
+text.forEach(function(str) {
+  textHtml.push(`<p>${str}</p>`);
+})
+return textHtml.join("");
+
+}
+
+
+// form
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  setLoadingItems();
+  // API
+  fetch(`https://api.openai.com/v1/completions`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${KEY_API}`,
+    },
+    body: JSON.stringify({
+      prompt: generateSentenceByAgeBudgetAndTheme(
+        ageInput.value,
+        budgetInput.value,
+        themeInput.value
+        ),
+      max_tokens: 2000,
+      model: "text-davinci-003",
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.choices[0].text);
+      footer.innerHTML = translateTextToHtml(data.choices[0].text);
+    })
+    .finally(() =>{
+      removeLoadingItems();
+    })
+  })
